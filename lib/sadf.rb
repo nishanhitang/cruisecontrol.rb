d17dea7d6b2ef69633205450d868e135d5e48b9f
Reccur Nishan <ogre41@gmail.com>
2014-09-23 11:39:42 +0545

    addes cap deploy feature

---
+++ lib/sadf.rb
@@ -0,0 +1,21 @@
+class CapistranoDeploy
+  attr_accessor :server
+
+  # Called when cruisecontrol starts and loads the plugin
+  def initialize(project = nil)
+    @server = ""
+    @project = project
+  end
+
+  # Runs when a build is finished (checkout, tests etc;)
+  def build_finished(build)
+    # If server isn't defined or the build fails
+    return if @server.empty? or build.failed?
+    # Otherwise run the command to deploy the code to the supplied server.
+    CommandLine.execute "cd #{@project.path}/work && cap #{@server} deploy:long"
+  rescue Exception
+    build.fail!("Couldn't deploy via cruisecontrol")
+  end
+end
+
+end
