fbe8c3c4d3f2cf3a83229afe24b61cd996e24fbe
Reccur Nishan <ogre41@gmail.com>
2014-09-23 19:08:51 +0545

    Added basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+	http_basic_authenticate_with name: "cruisecontrol", password: "password123"
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
